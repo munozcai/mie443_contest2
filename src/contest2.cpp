3126ef9762e2b50d8e13c8cf51d3efd20f9aeeac
talha <talha.javed@mail.utoronto.ca>
2020-03-12 11:14:44 -0400

    openCV working

--- src/contest2.cpp
+++ src/contest2.cpp
@@ -2,36 +2,79 @@
 #include <navigation.h>
 #include <robot_pose.h>
 #include <imagePipeline.h>
+bool matchFound = false;
+float result[5];
+int coordinateIndex;
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     // Setup ROS.
     ros::init(argc, argv, "contest2");
     ros::NodeHandle n;
     // Robot pose object + subscriber.
-    RobotPose robotPose(0,0,0);
+    RobotPose robotPose(0, 0, 0);
     ros::Subscriber amclSub = n.subscribe("/amcl_pose", 1, &RobotPose::poseCallback, &robotPose);
     // Initialize box coordinates and templates
-    Boxes boxes; 
-    if(!boxes.load_coords() || !boxes.load_templates()) {
+    Boxes boxes;
+    if (!boxes.load_coords() || !boxes.load_templates())
+    {
         std::cout << "ERROR: could not load coords or templates" << std::endl;
         return -1;
     }
-    for(int i = 0; i < boxes.coords.size(); ++i) {
+    for (int i = 0; i < boxes.coords.size(); ++i)
+    {
         std::cout << "Box coordinates: " << std::endl;
-        std::cout << i << " x: " << boxes.coords[i][0] << " y: " << boxes.coords[i][1] << " z: " 
+        std::cout << i << " x: " << boxes.coords[i][0] << " y: " << boxes.coords[i][1] << " z: "
                   << boxes.coords[i][2] << std::endl;
     }
     // Initialize image objectand subscriber.
     ImagePipeline imagePipeline(n);
+    
     // Execute strategy.
-    imagePipeline.imgObject(boxes);
-    while(ros::ok()) {
+    while (ros::ok())
+    {
         ros::spinOnce();
         /***YOUR CODE HERE***/
         // Use: boxes.coords
         // Use: robotPose.x, robotPose.y, robotPose.phi
-        imagePipeline.getTemplateID(boxes);
         
+
+        int ID = imagePipeline.getTemplateID(boxes);
+        std::cout << " template_id is: " << ID << std::endl;
+        if (!matchFound && ID != -1)
+        {
+            matchFound = true;
+            std::cout << " template_id is: " << ID << std::endl;
+            if (ID == 0)
+            {
+                std::cout << "Raisin Bran" << std::endl;
+                result[coordinateIndex]= ID;
+            }
+            else if (ID == 1)
+            {
+                std::cout << "Cinnamon Toast Crunch" << std::endl;
+                result[coordinateIndex]= ID;
+            } 
+            else if (ID == 2)
+            {
+                std::cout << "Rice Krispies " << std::endl;
+                result[coordinateIndex]= ID;
+            }
+
+            else if(ID == 3)
+            {
+                std::cout << "Blank " << std::endl;
+                result[coordinateIndex]= ID;
+            }
+          
+
+           break;
+        }
+        else
+        {
+            std::cout << " Match has not been found " << std::endl;
+        }
+
         ros::Duration(0.01).sleep();
     }
     return 0;
--- src/imagePipeline.cpp
+++ src/imagePipeline.cpp
@@ -10,23 +10,31 @@
 using namespace cv;
 using namespace cv::xfeatures2d;
 
-Mat img_object0;
-Mat img_object1;
-Mat img_object2;
-Mat img_scene;
-Mat descriptors_object0, descriptors_object1, descriptors_object2;
-Mat descriptors_scene;
-
-std::vector<KeyPoint> keypoints_object0, keypoints_object1, keypoints_object2;
-std::vector<KeyPoint> keypoints_scene;
+using namespace cv;
+using namespace cv::xfeatures2d;
+#define IMAGE_TYPE sensor_msgs::image_encodings::BGR8
+#define IMAGE_TOPIC "camera/image"
+//"camera/image" // kinect:"camera/rgb/image_raw" webcam:"camera/image"
 
 int minHessian = 400;
 Ptr<SURF> detector = SURF::create(minHessian);
-void descriptionInit();
-
-#define IMAGE_TYPE sensor_msgs::image_encodings::BGR8
-//#define IMAGE_TOPIC "camera/image" // kinect:"camera/rgb/image_raw" webcam:"camera/image"
-#define IMAGE_TOPIC "camera/image"
+Mat descriptors_object, descriptors_scene;
+Mat img_object;
+Mat img_scene;
+Mat img;
+Mat H;
+int maxGM = 0;
+int currentGM;
+int template_id = 3;
+std::vector<KeyPoint> keypoints_object;
+std::vector<KeyPoint> keypoints_scene;
+std::vector<Point2f> obj;
+std::vector<Point2f> scene;
+std::vector<std::vector<DMatch>> knn_matches;
+std::vector<DMatch> good_matches;
+std::vector<Point2f> obj_corners(4);
+std::vector<Point2f> scene_corners(4);
+//int templateIDArray[3] = {1, 1, 1};
 
 ImagePipeline::ImagePipeline(ros::NodeHandle &n)
 {
@@ -54,19 +62,118 @@ void ImagePipeline::imageCallback(const sensor_msgs::ImageConstPtr &msg)
     }
 }
 
-void ImagePipeline::imgObject(Boxes &boxes)
+int detectGM(int templateID)
 {
-    img_object0 = boxes.templates[0]; // raisin
-    img_object1 = boxes.templates[1];
-    img_object2 = boxes.templates[2];
-    descriptionInit();
+    knn_matches.clear();
+    good_matches.clear();
+    obj.clear();
+    scene.clear();
+    keypoints_object.clear();
+    keypoints_scene.clear();
+    Mat img_matches;
+    obj_corners.clear();
+    scene_corners.clear();
+    // H.clear();
+
+    detector->detectAndCompute(img_object, Mat(), keypoints_object,
+                               descriptors_object);
+    detector->detectAndCompute(img_scene, Mat(), keypoints_scene,
+                               descriptors_scene);
+
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+
+    matcher->knnMatch(descriptors_object, descriptors_scene, knn_matches, 2);
+
+    const float ratio_thresh = 0.75f;
+
+    for (size_t i = 0; i < knn_matches.size(); i++)
+    {
+        if (knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance)
+        {
+            good_matches.push_back(knn_matches[i][0]);
+        }
+    }
+
+    if (keypoints_scene.size() < 1)
+    {
+        std::cerr << "ISSUE MAYBE\n";
+        return -1;
+    }
+
+    if (good_matches.size() < 4)
+    {
+        std::cout << "Not enough good matches available in this image- Aldo" << std::endl;
+        //templateIDArray[templateID] = 0;
+        return -1;
+    }
+    else
+    {
+        drawMatches(img_object, keypoints_object, img_scene, keypoints_scene,
+                    good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+                    std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+        //-- Localize the object
+        //obj.clear();
+        //scene.clear();
+        std::vector<Point2f> obj_corners(4);
+        std::vector<Point2f> scene_corners(4);
+
+        for (int i = 0; i < good_matches.size(); i++)
+        {
+            //-- Get the keypoints from the good matches
+            obj.push_back(keypoints_object[good_matches[i].queryIdx].pt);
+            scene.push_back(keypoints_scene[good_matches[i].trainIdx].pt);
+        }
+
+        //std::cout << good_matches.size() << std::endl;
+
+        Mat H = findHomography(obj, scene, RANSAC);
+
+        if (H.empty())
+        {
+            return -1;
+        }
+
+        //-- Get the corners from the image_1 ( the object to be "detected" )
+
+        obj_corners[0] = cvPoint(0, 0);
+        obj_corners[1] = cvPoint(img_object.cols, 0);
+        obj_corners[2] = cvPoint(img_object.cols, img_object.rows);
+        obj_corners[3] = cvPoint(0, img_object.rows);
+
+        //debug
+        //std::cout << "obj " << obj_corners.size() << std::endl;
+        //std::cout << "Scene " << scene_corners.size() << std::endl;
+        // std::cout << "H " << H << std::endl;
+        perspectiveTransform(obj_corners, scene_corners, H);
+
+        //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+        line(img_matches, scene_corners[0] + Point2f(img_object.cols, 0),
+             scene_corners[1] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+        line(img_matches, scene_corners[1] + Point2f(img_object.cols, 0),
+             scene_corners[2] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+        line(img_matches, scene_corners[2] + Point2f(img_object.cols, 0),
+             scene_corners[3] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+        line(img_matches, scene_corners[3] + Point2f(img_object.cols, 0),
+             scene_corners[0] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
+
+        //-- Show detected matches
+        imshow("Good Matches & Object detection", img_matches);
+
+        return good_matches.size();
+
+        waitKey(0);
+    }
 }
+
 int ImagePipeline::getTemplateID(Boxes &boxes)
 {
-    int template_id = -1;
+    cv::waitKey(800);
+    img_scene = img;
+
     if (!isValid)
     {
         std::cout << "ERROR: INVALID IMAGE!" << std::endl;
+        return -1;
     }
     else if (img.empty() || img.rows <= 0 || img.cols <= 0)
     {
@@ -74,22 +181,53 @@ int ImagePipeline::getTemplateID(Boxes &boxes)
         std::cout << "img.empty():" << img.empty() << std::endl;
         std::cout << "img.rows:" << img.rows << std::endl;
         std::cout << "img.cols:" << img.cols << std::endl;
+        return -1;
     }
     else
     {
         /***YOUR CODE HERE***/
         // Use: boxes.templates
-        std::cout << "VALID IMAGE!" << std::endl;
-        // from template
+        template_id = 3;
+       
+        for (int i = 0; i < 3; i++)
+        {
 
-        //from image feed
-        img_scene = img;
-        getID();
+            img_object = boxes.templates[i];
+
+            if (!img_object.data || !img_scene.data)
+            {
+                std::cout << " --(!) Error reading images " << std::endl;
+                return -1;
+            }
+            else
+            {
+               
+                currentGM = detectGM(i);
+
+                //std::cout << " Number of Good Matches is: "<< currentGM << std::endl;
+                std::cout << " Number of Good Matches is: " << currentGM << std::endl;
+
+                if (maxGM < currentGM)
+                {
+                    maxGM = currentGM;
+                    template_id = i;
+                    std::cout << " Max Good Matches is: " << maxGM << std::endl;
+                }
+            }
+        }
 
         //cv::imshow("view", img);
+
         cv::waitKey(10);
     }
-    return template_id;
+    if (maxGM > 100)
+    {
+        return template_id;
+    }
+    else
+    {
+        return 3;
+    }
 }
 
 void descriptionInit()
